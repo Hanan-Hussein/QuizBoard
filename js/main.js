b3836a4aa204164c3f26c6f03e26029927d5f0e1
Hanan-Hussein <hanhuss035@gmail.com>
2022-02-06 01:57:34 +0300

    bg:form-values-comparison-records the first value only

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-console.log('help');
 var send_btn= document.getElementById('starttest');
+var question_btn=document.getElementById('questions_btn');
 send_btn.addEventListener('click',sendEvent);
+question_btn.addEventListener('click',calculationsEvent);
+
+
+var score =0;
+var answers=['Three','Function','onLoad()','head','Script']
 
 /**
  * This function saves the user's name to the local storage 
@@ -27,5 +32,20 @@ function sendEvent(e){
 
 }
 
+function calculationsEvent(e){
+    e.preventDefault();
+
+    var question_one= document.getElementsByName('action');
+
+    for(var i=0; i<question_one.length;i++){
+        if(question_one[i].checked){
+            console.log(question_one[i].value);
+        }
+        
+    }
+
+
+
+}
 
 
